31795fe34a7ac67bf9b1b8ce60dd08f0b85d1f70
Tontsu <Tontsu@Tontsus-MacBook-Air.local>
2015-12-07 21:36:34 +0200

    Lisäystesti

--- web/app/app.js
+++ web/app/app.js
@@ -1,1 +1,36 @@
 // Toteuta moduulisi tänne
+var MovieBase = angular.module('MyApp', ['firebase']);
+
+MovieBase.service('FirebaseService', function($firebaseArray){
+  
+    var firebaseRef = new Firebase('https://popping-torch-6063.firebaseio.com/movies');
+    var movies = $firebaseArray(firebaseRef);
+
+    this.getMovies = function(){
+        return movies;
+    }
+    
+    this.addMovies = function(movie){
+        movies.$add(data);
+    }
+});
+
+MovieBase.Controller('MovieBaseGetController', function($scope, FirebaseService) {
+    $scope.movies = FirebaseService.getMovies();
+    
+    
+});
+
+MovieBase.Controller('MovieBaseAddController', function($scope, FirebaseService) {
+    $scope.addMovies = function () {
+            FirebaseService.addMovies({
+                name: $scope.newName,
+                director: $scope.newDirector,
+                release: $scope.newRelease,
+                description: $scope.newDescription
+            });
+        
+    }
+    
+    
+});
