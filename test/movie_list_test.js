797a2ad9e3ea28fa15c2df6033cf0c5dba673f8d
Tontsu <Tontsu@Tontsus-MacBook-Air.local>
2015-12-09 23:44:43 +0200

    undefined

--- test/movie_list_test.js
+++ test/movie_list_test.js
@@ -1,77 +1,77 @@
-describe('Movie list', function(){
-	var controller, scope;
+describe('Movie list', function () {
+    var controller, scope;
 
-	var FirebaseServiceMock;
+    var FirebaseServiceMock;
 
-  	beforeEach(function(){
-  		// Lisää moduulisi nimi tähän
-    	module('MovieBase');
+    beforeEach(function () {
+        // Lisää moduulisi nimi tähän
+        module('MovieBase');
 
-    	FirebaseServiceMock = (function(){
-            
-        var movies = [
-        {
-            name: "trolli",
-            director: "röli",
-            release: "2001",
-            description: "röllimies"
-        },
-        {
-           name: "trolli2",
-            director: "röli2",
-            release: "2001",
-            description: "röllimies2"
-        },
-        {
-           name: "trolli3",
-            director: "röli3",
-            release: "2001",
-            description: "röllimies3"
-        }
-      ];
-			return {
-				// Toteuta FirebaseServicen mockatut metodit tähän
-                                getMovies: function() {
-                                    return movies;
-                                }
-			}
-		})();
+        FirebaseServiceMock = (function () {
 
-		// Lisää vakoilijat
-	    // spyOn(FirebaseServiceMock, 'jokuFunktio').and.callThrough();
-            spyOn(FirebaseServiceMock, 'getMovies').and.callThrough();
+            var movies = [
+                {
+                    name: "trolli",
+                    director: "röli",
+                    release: "2001",
+                    description: "röllimies"
+                },
+                {
+                    name: "trolli2",
+                    director: "röli2",
+                    release: "2001",
+                    description: "röllimies2"
+                },
+                {
+                    name: "trolli3",
+                    director: "röli3",
+                    release: "2001",
+                    description: "röllimies3"
+                }
+            ];
+            return {
+                // Toteuta FirebaseServicen mockatut metodit tähän
+                getMovies: function () {
+                    return movies;
+                }
+            }
+        })();
 
-    	// Injektoi toteuttamasi kontrolleri tähän
-	    inject(function($controller, $rootScope) {
-	      scope = $rootScope.$new();
-	      // Muista vaihtaa oikea kontrollerin nimi!
-	      controller = $controller('MovieBaseGetController', {
-	        $scope: scope,
-	        FirebaseService: FirebaseServiceMock
-	      });
-	    });
-  	});
+        // Lisää vakoilijat
+        // spyOn(FirebaseServiceMock, 'jokuFunktio').and.callThrough();
+        spyOn(FirebaseServiceMock, 'getMovies').and.callThrough();
 
-  	/*
-  	* Testaa alla esitettyjä toimintoja kontrollerissasi
-  	*/
+        // Injektoi toteuttamasi kontrolleri tähän
+        inject(function ($controller, $rootScope) {
+            scope = $rootScope.$new();
+            // Muista vaihtaa oikea kontrollerin nimi!
+            controller = $controller('MovieBaseGetController', {
+                $scope: scope,
+                FirebaseService: FirebaseServiceMock
+            });
+        });
+    });
 
-  	/*
-  	* Testaa, että Firebasesta (mockilta) saadut elokuvat löytyvät konrollerista
-  	* Testaa myös, että Firebasea käyttävästä palvelusta kutsutaan oikeaa funktiota,
-  	* käyttämällä toBeCalled-oletusta.
-  	*/ 
-	it('should list all movies from the Firebase', function(){
-		expect(scope.movies.length).toBe(3);
-                expect(FirebaseServiceMock.getMovies).toHaveBeenCalled();
-	});
+    /*
+     * Testaa alla esitettyjä toimintoja kontrollerissasi
+     */
 
-	/* 
-	* Testaa, että elokuvan pystyy poistamaan Firebasesta.
-	* Testaa myös, että Firebasea käyttävästä palvelusta kutsutaan oikeaa funktiota,
-  	* käyttämällä toBeCalled-oletusta.
-	*/
-	it('should be able to remove a movie', function(){
-		expect(true).toBe(false);
-	});
+    /*
+     * Testaa, että Firebasesta (mockilta) saadut elokuvat löytyvät konrollerista
+     * Testaa myös, että Firebasea käyttävästä palvelusta kutsutaan oikeaa funktiota,
+     * käyttämällä toBeCalled-oletusta.
+     */
+    it('should list all movies from the Firebase', function () {
+        expect(scope.movies.length).toBe(3);
+        expect(FirebaseServiceMock.getMovies).toHaveBeenCalled();
+    });
+
+    /* 
+     * Testaa, että elokuvan pystyy poistamaan Firebasesta.
+     * Testaa myös, että Firebasea käyttävästä palvelusta kutsutaan oikeaa funktiota,
+     * käyttämällä toBeCalled-oletusta.
+     */
+    it('should be able to remove a movie', function () {
+        expect(true).toBe(false);
+    });
 });
